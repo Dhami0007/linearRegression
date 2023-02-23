98009dab33e80e03f7d8eacdeb4353c28e41cc2e
Jaspreet Singh Dhami <jdhami1@ualberta.ca>
2023-02-22 23:43:47 -0700

    made for one variable
    made the code for one variable assuming only one variable influences the score of the person.

---
+++ scorePredictor.py
@@ -0,0 +1,48 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+data = pd.read_csv('score.csv')
+
+def loss_function(m, b, points):
+    """ this function is calculating the mean squared error based on the arguements passed to it """
+    error = 0 
+    for i in range(len(points)):
+        x = points.iloc[i].Hours
+        y = points.iloc[i].Scores
+        error += (y - (m * x + b)) ** 2
+    error = error / len(points)
+
+def gradient_descent(m_now, b_now, points, L):
+    m_gradient = 0
+    b_gradient = 0
+
+    n = len(points)
+
+    for i in range(n):
+        x = points.iloc[i].Hours
+        y = points.iloc[i].Scores
+
+        m_gradient += -(2/n)*x* (y - (m_now*x + b_now))
+        b_gradient += -(2/n) * (y - (m_now*x + b_now))
+    
+    m = m_now - m_gradient * L
+    b = b_now - b_gradient * L
+    return m, b
+
+m = 0
+b = 0
+L = 0.0001  # Learning rate
+epochs = 1000   # iterations
+
+for i in range(epochs):
+    if i%100 == 0:
+        print("Epoch: " + str(i))
+    m, b = gradient_descent(m, b, data, L)
+
+print(m, b)
+
+plt.scatter(data.Hours, data.Scores, color="black")
+plt.plot(list(range(2,10)), [m*x + b for x in range(2,10)], color="red")
+plt.show()
+
+    
